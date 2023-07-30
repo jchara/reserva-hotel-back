b17d1b3441807299a58bb18c60744723a721ac7d
Janier Chara Cardona <jchara08@gmail.com>
2023-07-29 19:37:39 -0500

    feat: add cors

--- src/main/java/gia/utp/primerproyecto/primerproyecto/web/controller/RoomController.java
+++ src/main/java/gia/utp/primerproyecto/primerproyecto/web/controller/RoomController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin({"*"})
 @RestController
 @RequestMapping("v1/rooms")
 public class RoomController {
--- src/main/java/gia/utp/primerproyecto/primerproyecto/web/controller/UserController.java
+++ src/main/java/gia/utp/primerproyecto/primerproyecto/web/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin({"*"})
 @RestController
 @RequestMapping("v1/users")
 public class UserController {
